7b32849907a771e6dc4ff995fc968e32856f5888
BMFreed <badmofo14@gmail.com>
2021-08-13 12:51:31 +0300

    Header burger menus
    There are now 2 working menus in the header.
    The logic part is realised via hooks. Opening each menu causes another to close, they both close when entering desktop mode, etc etc.
    
    No additional DOM elements were added to implement these features.

--- src/common.blocks/Header/Header.js
+++ src/common.blocks/Header/Header.js
@@ -1,10 +1,62 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 function Header() {
-    const [open, setOpen] = useState(true);
+    const [openNav, setOpenNav] = useState(false);
+    const [openLinks, setOpenLinks] = useState(false);
+    const [width, setWidth] = useState(window.innerWidth);
+    const navRef = useRef(null);
+    const linksRef = useRef(null);
+    const hamburgerIcon = process.env.PUBLIC_URL + "/img/ui/menu.png";
+    const closeIcon = process.env.PUBLIC_URL + "/img/ui/close.png";
 
-    const handleMenu = () => {
-        setOpen(!open);
+
+    //Now we will tell React to monitor the screen width and set the behaviour of the menus accordingly.
+    useEffect(() => {
+        function handleResize() {
+            setWidth(window.innerWidth);
+        }
+
+        window.addEventListener("resize", handleResize);
+        if (width <= 1100) {
+            if (openNav) {
+                navRef.current.style.transform = "translateY(0px)";
+            }
+            else {
+                navRef.current.style.transform = "translateY(-280px)";
+            }
+            if (openLinks) {
+                linksRef.current.style.transform = "translateY(0px)";
+            } else {
+                linksRef.current.style.transform = "translateY(-280px)";
+            }
+        } else {
+            navRef.current.style.transform = "translateY(0px)";
+            linksRef.current.style.transform = "translateY(0px)";
+            setOpenNav(false);
+            setOpenLinks(false);
+            //This is done so that all the menus close when entering desktop mode.
+        }
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, [width, openNav, openLinks]);
+
+    const handleOpenNav = () => {
+        if(!openNav) {
+            setOpenNav(true);
+            setOpenLinks(false);
+        }
+        else if (openNav) {
+            setOpenNav(false);
+        }
+    };
+
+    const handleOpenLinks = () => {
+        if (!openLinks) {
+            setOpenLinks(true);
+            setOpenNav(false);
+        } else if (openLinks) {
+            setOpenLinks(false);
+        }
     };
 
     return (
@@ -13,38 +65,50 @@ function Header() {
                 <div className="header__left-content">
                     <button
                         className="header__burger-menu header__mobile-menu"
-                        onClick={handleMenu}
+                        onClick={handleOpenNav}
                     >
-                        <img src={process.env.PUBLIC_URL + "/img/ui/menu.png"} alt="" />
+                        <img src={openNav ? closeIcon : hamburgerIcon} alt="" />
                     </button>
                     <h1 className="header__title header-cursive margin-reset">
                         <a className="link" href="index.html">
                             Studio
                         </a>
                     </h1>
-                    <ul className="header__section-list u-list">
+                    <ul className="header__section-list u-list" ref={navRef}>
                         <li>
-                            <a className="link" href="#Individual Sessions">
+                            <a
+                                className="link"
+                                href="#Individual Sessions"
+                                onClick={() => setOpenNav(!openNav)}
+                            >
                                 Individuals
                             </a>
                         </li>
                         <li>
-                            <a className="link" href="#Group Sessions">
+                            <a
+                                className="link"
+                                href="#Group Sessions"
+                                onClick={() => setOpenNav(!openNav)}
+                            >
                                 Companies
                             </a>
                         </li>
                         <li>
-                            <a className="link" href="#services">
+                            <a
+                                className="link"
+                                href="#services"
+                                onClick={() => setOpenNav(!openNav)}
+                            >
                                 The Studio
                             </a>
                         </li>
                         <li>
-                            <a className="link" href="#about">
+                            <a className="link" href="#about" onClick={() => setOpenNav(!openNav)}>
                                 About Us
                             </a>
                         </li>
                         <li>
-                            <a className="link" href="#blog">
+                            <a className="link" href="#blog" onClick={() => setOpenNav(!openNav)}>
                                 Blog
                             </a>
                         </li>
@@ -54,9 +118,13 @@ function Header() {
                     <a className="header__btn button link" href="##">
                         Book Now
                     </a>
-                    <ul className="header__sm-list u-list">
-                        <li>
-                            <a href="##">
+                    <button className="header__mobile-menu header__links" onClick={handleOpenLinks}>
+                        Links
+                    </button>
+                    <ul className="header__sm-list u-list" ref={linksRef}>
+                        <li onClick={() => setOpenLinks(!openLinks)}>
+                            <a href="##" className="link header__sm-link">
+                                <span>Facebook</span>
                                 <img
                                     src={process.env.PUBLIC_URL + "/img/social_media/fb_11189.png"}
                                     alt="Facebook"
@@ -64,7 +132,12 @@ function Header() {
                             </a>
                         </li>
                         <li>
-                            <a href="##">
+                            <a
+                                href="##"
+                                className="link header__sm-link"
+                                onClick={() => setOpenLinks(!openLinks)}
+                            >
+                                <span>Instagram</span>
                                 <img
                                     src={process.env.PUBLIC_URL + "/img/social_media/in_11190.png"}
                                     alt="Instagram"
@@ -72,7 +145,12 @@ function Header() {
                             </a>
                         </li>
                         <li>
-                            <a href="##">
+                            <a
+                                href="##"
+                                className="link header__sm-link"
+                                onClick={() => setOpenLinks(!openLinks)}
+                            >
+                                <span>LinkedIn</span>
                                 <img
                                     src={process.env.PUBLIC_URL + "/img/social_media/li_11191.png"}
                                     alt="LinkedIn"
@@ -80,7 +158,12 @@ function Header() {
                             </a>
                         </li>
                         <li>
-                            <a href="##">
+                            <a
+                                href="##"
+                                className="link header__sm-link"
+                                onClick={() => setOpenLinks(!openLinks)}
+                            >
+                                <span>Yelp</span>
                                 <img
                                     src={process.env.PUBLIC_URL + "/img/social_media/yp_11192.png"}
                                     alt="Yelp"
@@ -88,7 +171,12 @@ function Header() {
                             </a>
                         </li>
                         <li>
-                            <a href="##">
+                            <a
+                                href="##"
+                                className="link header__sm-link"
+                                onClick={() => setOpenLinks(!openLinks)}
+                            >
+                                <span>Google</span>
                                 <img
                                     src={
                                         process.env.PUBLIC_URL + "/img/social_media/ggle_11193.png"
@@ -98,7 +186,6 @@ function Header() {
                             </a>
                         </li>
                     </ul>
-                    <button className="header__mobile-menu header__links">Links</button>
                 </div>
             </nav>
         </header>
